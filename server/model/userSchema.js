5afc627cc00e9ce4b3fc4334733a366e7c1ce707
dragonfist630 <gadharinayan@gmail.com>
2022-03-21 18:33:09 +0530

    fixed index file

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -17,12 +17,18 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true
-    }
+    },
+    queryName: [
+        {
+          type: String,
+          required: true
+        },
+      ],
 });
 
 //here implementing pre middleware which will run before save()
 userSchema.pre('save', async function (next){
-    console.log("hello from inside pre function");
+    // console.log("hello from inside pre function");
     //isModified is a function which will pass true if the string passed inside it will be modified
     if(this.isModified('password')){
         this.password = await bycrpyt.hash(this.password, 12); // hash takes the variable v want to hash and number of rounds.
--- server/router/auth.js
+++ server/router/auth.js
@@ -7,12 +7,12 @@ require("../DB/conn.js");
 const Usr = require("../model/userSchema.js");
 const Query = require("../model/querySchema.js");
 
-router.get("/",cors(), (req, res) => {
+router.get("/", cors(), (req, res) => {
   res.send("hello there!!!");
 });
 
 //for registration
-router.post("/reg",cors(), async (req, res) => {
+router.post("/reg", cors(), async (req, res) => {
   const { firstName, lastName, emailId, password } = req.body;
   if (!firstName || !lastName || !emailId || !password) {
     return res.status(422).json({ error: "Please fill all the details." });
@@ -22,7 +22,7 @@ router.post("/reg",cors(), async (req, res) => {
     if (userExist) {
       return res.status(422).json({ error: "Email already exists." });
     }
-    const unuser = new Usr({ firstName, lastName, emailId, password });
+    const unuser = new Usr({ firstName, lastName, emailId, password, queryName: [] });
     await unuser.save();
     res.status(201).json({ message: "User register successfully." });
   } catch (err) {
@@ -30,7 +30,7 @@ router.post("/reg",cors(), async (req, res) => {
   }
 });
 
-router.post("/login",cors(), async (req, res) => {
+router.post("/login", cors(), async (req, res) => {
   const { emailId, password } = req.body;
   if (!emailId || !password) {
     return res.status(422).json({ error: "Please enter all the fields." });
@@ -51,33 +51,51 @@ router.post("/login",cors(), async (req, res) => {
   }
 });
 
-router.post("/querys",cors(), async (req, res) => {
-  const { queryName, optionName, value, totalVotes } = req.body;
-  console.log(req.body);
-  console.log(optionName, value);
-//   if (!queryName || !optionName || !value || !totalVotes) {
-//     return res.status(422).json({ error: "Please entered every field." });
-//   }
-  const queryDetails = await Query.findOne({ queryName: queryName });
+router.post("/voteQueries", cors(), async (req, res) => {
+  const { userId, queryName } = req.body;
+  if (!userId || !queryName) {
+    return res.status(422).json({ error: "Please provide details" });
+  }
   try {
-    if (queryDetails) {
-      return res.status(422).json({ error: "This query already exist." });
+    const userExists = await Usr.findOne({_id: userId });
+    if (userExists) {
+      console.log(userExists);
+      userExists.queryName.push(queryName);
+      userExists.save();
+      return res.status(201).json({ message: "Vote Uploaded." });
     }
-    const qry = new Query({queryName, optionName, value, totalVotes});
-    await qry.save();
-    res.status(201).json({message:"This query added successfully"});
+    else{
+    res.status(422).json({ error: "User Doesn't exists." });
+    }
   } catch (error) {
     console.log(error);
   }
 });
 
-router.get("/getquery",cors(), async (req,res)=>{
-    const temp = await Query.find();
-    try{
-    res.status(201).json(temp);
-    }catch(e){
-        console.log(e);
+router.post("/querys", cors(), async (req, res) => {
+  const { queryName, optionName, value, totalVotes } = req.body;
+  console.log(req.body);
+  console.log(optionName, value);
+  const queryDetails = await Query.findOne({ queryName: queryName });
+  try {
+    if (queryDetails) {
+      return res.status(422).json({ error: "This query already exist." });
     }
+    const qry = new Query({ queryName, optionName, value, totalVotes });
+    await qry.save();
+    res.status(201).json({ message: "This query added successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/getquery", cors(), async (req, res) => {
+  const temp = await Query.find();
+  try {
+    res.status(201).json(temp);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
